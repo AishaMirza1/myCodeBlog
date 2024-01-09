58350cd73690e955be6460e3129816f283552c46
Home <aishamirza0002@gmail.com>
2024-01-09 23:00:21 +0530

    add:menu1 Article transition

--- src/pages/article/articles/animations/Menu/Menu1.jsx
+++ src/pages/article/articles/animations/Menu/Menu1.jsx
@@ -5,12 +5,19 @@ import PropTypes from "prop-types";
 export default function Menu1() {
   const [isActive, setIsActive] = useState(false);
   const variants = {
-    open: { width: 480, height: 550, top: "-25px", right: "-25px" },
+    open: {
+      width: 480,
+      height: 550,
+      top: "-25px",
+      right: "-25px",
+      transition: { duration: 0.75, ease: [0.76, 0, 0.24, 1] },
+    },
     closed: {
       width: 100,
       height: 40,
       top: 0,
       right: 0,
+      transition: { duration: 0.75, ease: [0.76, 0, 0.24, 1] },
     },
   };
   return (
