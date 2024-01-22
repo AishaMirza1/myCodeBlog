aa7d27d6bc93da6fcbff1f2541254532d7a560ea
Home <aishamirza0002@gmail.com>
2024-01-22 22:41:36 +0530

    feat:headin1 animation using ramer motion

--- src/ui/Heading1.jsx
+++ src/ui/Heading1.jsx
@@ -1,7 +1,33 @@
 import styles from "./ui.module.css";
 import PropTypes from "prop-types";
+import { motion } from "framer-motion";
 export default function Heading1({ text }) {
-  return <h1 className={styles.heading1}>{text}</h1>;
+  const variants = {
+    initial: {
+      opacity: 0,
+      y: -5,
+    },
+    enter: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+  return (
+    <motion.h1
+      className={styles.heading1}
+      transition={{ staggerChildren: 0.1 }}
+      initial="initial"
+      animate="enter"
+    >
+      {text.split("").map((char, i) => {
+        return (
+          <motion.span variants={variants} key={i}>
+            {char}
+          </motion.span>
+        );
+      })}
+    </motion.h1>
+  );
 }
 
 Heading1.propTypes = {
