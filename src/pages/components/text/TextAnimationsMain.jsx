67af8b27b58fa011ee0046365aaf7a3828c4790e
Home <aishamirza0002@gmail.com>
2024-01-24 13:10:06 +0530

    feat:new text animation components added

--- src/pages/components/text/TextAnimationsMain.jsx
+++ src/pages/components/text/TextAnimationsMain.jsx
@@ -1,11 +1,39 @@
 import styles from "./textAnimationsMain.module.css";
 import MainComponentsHeader from "../../../ui/MainComponentsHeader";
-import TextAnimations from "./flickeringText/TextAnimations";
+import TextAnimations, {
+  PopTextAnimation,
+} from "./flickeringText/TextAnimations";
+import { InclineTextAnimation } from "./flickeringText/TextAnimations";
 export default function TextAnimationsMain() {
   return (
     <main className={styles.textAnimations}>
       <MainComponentsHeader heading="TextAnimations" />
-      <TextAnimations />
+      <InclineTextAnimation text="LONDON" />
+      <PopTextAnimation text="MUMBAI" />
+      <TextAnimations
+        text="I'm your Text"
+        variants={{
+          initial: { opacity: 0, x: 100 },
+          enter: { opacity: 1, x: 0 },
+          exit: { opacity: 1, x: 0 },
+        }}
+      />
+      <TextAnimations
+        text="another text"
+        variants={{
+          initial: { opacity: 0, y: 4 },
+          enter: { opacity: 1, y: 0 },
+          exit: { opacity: 1, y: 0 },
+        }}
+      />
+      <TextAnimations
+        text="another text"
+        variants={{
+          initial: { opacity: 0, rotateX: 180 },
+          enter: { opacity: 1, rotateX: 0 },
+          exit: { opacity: 1, rotateX: 0 },
+        }}
+      />
     </main>
   );
 }
--- src/pages/components/text/flickeringText/TextAnimations.jsx
+++ src/pages/components/text/flickeringText/TextAnimations.jsx
@@ -1,45 +1,177 @@
 import { useState } from "react";
 import styles from "./textAnimations.module.css";
 import { motion } from "framer-motion";
-export default function TextAnimations() {
-  const text = "I'm your Text";
-  const [start, setStart] = useState(false);
+import PropTypes from "prop-types";
+export default function TextAnimations({ text, variants }) {
+  const [start, setStart] = useState(true);
   return (
     <div className={styles.textAnimationsContainer}>
-      <div className={styles.textBox}>
-        {start && (
-          <motion.p
-            transition={{
-              staggerChildren: 0.05,
-              repeat: "infinity",
-              ease: "easeInOut",
-            }}
-            initial="initial"
-            animate={start && "enter"}
-          >
-            {text.split("").map((char, i) => {
-              return (
-                <motion.span
-                  style={{ display: "inline-block" }}
-                  variants={{
-                    initial: { opacity: 0, x: 100 },
-                    enter: { opacity: 1, x: 0 },
-                  }}
-                  key={i}
-                >
-                  {char}
-                </motion.span>
-              );
-            })}
-          </motion.p>
-        )}
-        <button
-          className={styles.animationBtn}
-          onClick={() => setStart((prev) => !prev)}
+      <div
+        onMouseEnter={() => {
+          setStart(true);
+        }}
+        onMouseLeave={() => {
+          setStart(false);
+        }}
+        className={styles.textBox}
+        style={{ flexDirection: "column" }}
+      >
+        <motion.p
+          transition={{
+            staggerChildren: 0.15,
+            ease: "easeInOut",
+          }}
+          initial="initial"
+          animate={start ? "enter" : ""}
         >
-          Start
-        </button>
+          {text.split("").map((char, i) => {
+            return (
+              <motion.span
+                style={{ display: "inline-block" }}
+                variants={variants}
+                key={i}
+                transition={{ ease: "linear" }}
+              >
+                {char}
+              </motion.span>
+            );
+          })}
+        </motion.p>
+        <p className={styles.hovermeText}>Hover Me</p>
       </div>
     </div>
   );
 }
+
+function InclineTextAnimation({ text }) {
+  const [start, setStart] = useState(false);
+  const variants = {
+    initinal: { y: 0 },
+    enter: (i) => ({
+      y: "-5.8rem",
+      transition: {
+        duration: 0.2,
+        delay: i * 0.098,
+        ease: [0.5, 0.465, 0.87, 0.9],
+      },
+    }),
+    exit: () => ({
+      y: 0,
+      transition: { duration: 0.3 },
+    }),
+  };
+  return (
+    <div className={styles.textAnimationsContainer}>
+      <div
+        className={styles.textBox}
+        onMouseEnter={() => {
+          setStart(true);
+        }}
+        onMouseLeave={() => {
+          setStart(false);
+        }}
+      >
+        <motion.p>
+          {text.split("").map((char, i) => {
+            return (
+              <motion.span
+                key={i}
+                style={{
+                  display: "inline-block",
+                  overflow: "hidden",
+                  height: "5rem",
+                  position: "relative",
+                }}
+              >
+                <motion.span
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                  initial="initial"
+                  animate={start ? "enter" : "exit"}
+                  custom={i}
+                  variants={variants}
+                >
+                  <span
+                    style={{
+                      display: "inline-block",
+                      fontSize: "5rem",
+                    }}
+                  >
+                    {char}
+                  </span>
+                  <span
+                    style={{
+                      display: "inline-block",
+                      fontSize: "5rem",
+                    }}
+                  >
+                    {char}
+                  </span>
+                </motion.span>
+              </motion.span>
+            );
+          })}
+        </motion.p>
+        <p className={styles.hovermeText}>Hover Me</p>
+      </div>
+    </div>
+  );
+}
+function PopTextAnimation({ text }) {
+  const [start, setStart] = useState(false);
+  const variants = {
+    initinal: { y: 0 },
+    enter: (i) => ({
+      scaleY: [1, 1.1, 1.2, 1.3, 1.4, 1.3, 1.2, 1.1, 1],
+      transition: { duration: 0.3, delay: i * 0.1 },
+    }),
+  };
+  return (
+    <div className={styles.textAnimationsContainer}>
+      <div
+        className={styles.textBox}
+        onMouseEnter={() => {
+          setStart(true);
+        }}
+        onMouseLeave={() => {
+          setStart(false);
+        }}
+      >
+        <motion.p>
+          {text.split("").map((char, i) => {
+            return (
+              <motion.span
+                key={i}
+                style={{
+                  display: "inline-block",
+                  fontSize: "5rem",
+                }}
+                initial="initial"
+                animate={start ? "enter" : ""}
+                exit="exit"
+                custom={i}
+                variants={variants}
+              >
+                {char}
+              </motion.span>
+            );
+          })}
+        </motion.p>
+        <p className={styles.hovermeText}>Hover Me</p>
+      </div>
+    </div>
+  );
+}
+TextAnimations.propTypes = {
+  text: PropTypes.string,
+  variants: PropTypes.object,
+};
+InclineTextAnimation.propTypes = {
+  text: PropTypes.string,
+};
+PopTextAnimation.propTypes = {
+  text: PropTypes.string,
+};
+export { InclineTextAnimation, PopTextAnimation };
